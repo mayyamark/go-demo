2ac410d265962271ba5f5f304f8a522295bd7885
-g <-g>
2021-10-29 13:53:16 +0300

    Add usage of an external package

--- hello/hello.go
+++ hello/hello.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 
 	"github.com/mayyamark/go-demo/greetings"
+	"rsc.io/quote"
 )
 
 func main() {
 	// Get a greeting message and print it.
-	message := greetings.Hello("Mayya")
+	name := "Mayya"
+	message := greetings.Hello(name)
 	fmt.Println(message)
+
+	fmt.Println(quote.Go())
 }
