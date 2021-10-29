9fd34f2f91ecde080103a06d369d317740e97e32
-g <-g>
2021-10-29 16:09:25 +0300

    Greet many people

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -7,12 +7,30 @@ import (
 	"time"
 )
 
+// Hello returns a greeting for the named person
 func Hello(name string) (string, error) {
 	if name == "" {
-		return "", errors.New("passed an empty name")
+		return "", errors.New("passed an empty name to Hello")
 	}
 
 	return fmt.Sprintf(randomFormat(), name), nil
+}
+
+// Hellos returns a map that associates each of the named people with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling the Hello function to get a random message for each name.
+	for _, name := range names { // range returns the index and a copy of the value
+		message, err := Hello(name)
+
+		if err != nil {
+			return nil, err
+		}
+		// In the map, associate the retrieved message with the name.
+		messages[name] = message
+	}
+
+	return messages, nil
 }
 
 // The init function seeds  the rand package with the current time.
@@ -33,7 +51,6 @@ func randomFormat() string {
 		"Hail, %v! Well met!",
 	}
 
-	// Return a randomly selected message format by specifying
-	// a random index for the slice of formats.
+	// Return a randomly selected message format by specifying a random index for the slice of formats.
 	return formats[rand.Intn(len(formats))]
 }
--- hello/hello.go
+++ hello/hello.go
@@ -9,25 +9,24 @@ import (
 )
 
 func main() {
-	// Set properties of the predefined Logger, including
-	// the log entry prefix and a flag to disable printing
-	// the time, source file, and line number.
-	log.SetPrefix("greetings: ")
-	log.SetFlags(0)
+	// Set properties of the predefined Logger, including:
+	log.SetPrefix("greetings: ") // the log entry prefix,
+	log.SetFlags(0)              // a flag to disable printing the time, source file, and line number.
 
 	// Request for a greeting message.
-	// message, err := greetings.Hello(name) // This would return error:		greetings: passed an empty name
+	// message, err := greetings.Hello(name) // This would return error:		greetings: passed an empty name to Hello
 	message, err := greetings.Hello("Mayya")
+	messages, err2 := greetings.Hellos([]string{"Ivan", "Mayya", "Martin"})
 
-	// If an error was returned, print it to the console and
-	// exit the program.
+	// If an error was returned, print it to the console and exit the program.
 	if err != nil {
 		log.Fatal(err)
 	}
+	if err2 != nil {
+		log.Fatal(err2)
+	}
 
-	// If no error was returned, print the returned message
-	// to the console.
 	fmt.Println(message)
-	// And the quote as well.
+	fmt.Println(messages)
 	fmt.Println(quote.Go())
 }
